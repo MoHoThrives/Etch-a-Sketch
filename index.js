27922a1cd178f5654a44ecb6f1953641803d608b
Mohanad Osman <mohanadhmd@gmail.com>
2020-02-01 02:43:59 -0500

    Pen Trailmark effect made

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const mainDivWindow = document.querySelector("#window");
 
+let spillInk = e => {
+    e.target.style.backgroundColor = "blue";
+}
+
 for (let row = 0; row < 16; row++) {
     for (let col = 0; col < 16; col++) {
         const boxI = document.createElement("div");
         boxI.classList.add('grid-box');
         mainDivWindow.appendChild(boxI);
+        boxI.addEventListener("mouseover", spillInk)
     }
 }
+
+
