f322326b085b5da3f50f632b578713ca5fbfbb72
Mohanad Osman <mohanadhmd@gmail.com>
2020-02-02 01:30:37 -0500

    User can now adjust the number of pixels in the grid. Grid was also resized to 1200px

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+
+
 const mainDivWindow = document.querySelector("#window");
+console.log(mainDivWindow.style.width);
+const resizeButton = document.querySelector("#gridPixelize");
 
 let spillInk = e => {
     e.target.style.backgroundColor = "blue";
-}
+};
 
-for (let row = 0; row < 16; row++) {
-    for (let col = 0; col < 16; col++) {
-        const boxI = document.createElement("div");
-        boxI.classList.add('grid-box');
-        mainDivWindow.appendChild(boxI);
-        boxI.addEventListener("mouseover", spillInk)
+
+
+function fillGrid(gridSize) {
+    for (let row = 0; row < gridSize; row++) {
+        for (let col = 0; col < gridSize; col++) {
+            const boxI = document.createElement("div");
+            let widthAndHeight = (1200/gridSize)-2;
+            boxI.style.width = widthAndHeight.toString()+"px";
+            boxI.style.height = widthAndHeight.toString()+"px";
+            boxI.style.borderColor = "gray";
+            boxI.style.borderWidth = "1px";
+            boxI.style.borderStyle = "dotted";
+            mainDivWindow.append(boxI);
+            boxI.addEventListener("mouseover", spillInk)
+        }
     }
 }
+
+fillGrid(16);
+resizeButton.addEventListener("mousedown", function () {
+    mainDivWindow.innerHTML = "";
+    let numPixels = prompt("Please enter new grid width?");
+    numPixels = parseInt(numPixels);
+    fillGrid(numPixels);
+});
 
 
